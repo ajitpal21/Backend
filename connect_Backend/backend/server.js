f6d3d77b22ddae7bed79ab193e54c0a425268de0
Ajit Pal <ajitpalbbt@gmail.com>
2025-10-28 02:19:05 +0530

    try to connect frontend with backend

---
+++ connect_Backend/backend/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Server is Ready');
+});
+
+  //get a list of 5 jokes
+   app.get('/jokes', (req, res) => {
+    const jokes = [ 
+      {
+         id: 1,
+        title: 'A joke',
+        content: 'This is a joke'      
+        },
+
+        {
+         id: 2,
+        title: 'Another joke',
+        content: 'This is another joke'      
+        },
+
+        {
+         id: 3,
+        title: 'A third joke',
+        content: 'This is a third joke'      
+        },
+        
+        {
+         id: 4,
+        title: 'A fourth joke',
+        content: 'This is a fourth joke'      
+        },
+
+        {
+         id: 5,
+        title: 'A fifth joke',
+        content: 'This is a fifth joke'      
+        },
+    ];
+       res.send(jokes);
+  });
+    
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
