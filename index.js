c3af7b7d7e28258354bf2966b29cf041910320e1
Ajit Pal <ajitpalbbt@gmail.com>
2025-10-27 17:26:13 +0530

    Github data added

--- index.js
+++ index.js
@@ -4,6 +4,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const GithubData = {
+  "login": "ajitpal21",
+  "id": 202176710,
+  "node_id": "U_kgDODAz4xg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/202176710?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/ajitpal21",
+  "html_url": "https://github.com/ajitpal21",
+  "followers_url": "https://api.github.com/users/ajitpal21/followers",
+  "following_url": "https://api.github.com/users/ajitpal21/following{/other_user}",
+  "gists_url": "https://api.github.com/users/ajitpal21/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/ajitpal21/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/ajitpal21/subscriptions",
+  "organizations_url": "https://api.github.com/users/ajitpal21/orgs",
+  "repos_url": "https://api.github.com/users/ajitpal21/repos",
+  "events_url": "https://api.github.com/users/ajitpal21/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/ajitpal21/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 4,
+  "public_gists": 0,
+  "followers": 1,
+  "following": 1,
+  "created_at": "2025-03-06T19:32:40Z",
+  "updated_at": "2025-09-20T11:15:15Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -24,6 +60,10 @@ app.get('/pal', (req, res) => {
     res.send('<h2>I am influencer.</h2>')
 })
 
-app.listen(port, () => {
+app.get('/github', (req, res) => {
+    res.json(GithubData)
+})
+
+app.listen(process.env.port, () => {
   console.log(`Example app listening on port ${port}`)
 })
